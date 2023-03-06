4e905c151bbbe9ff15afd0144dee879e5cfb1786
AndrewYako <andrejs.jakovlevs.7@inbox.lv>
2023-03-06 15:22:39 +0200

    06.03

---
+++ box/web/ask/qa/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse 
+
+def test(request, *args, **kwargs):
+    return HttpResponse('OK')
+
+def detail(request, qa_id):
+    return HttpResponse(qa_id)
--- box/web/init.sh
+++ box/web/init.sh
@@ -1,4 +1,13 @@
+sudo cd /home/box/web/ask
+sudo pip3 install django
+sudo pip3 install gunicorn
+sudo apt update
+sudo apt upgrade
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
+sudo chmod 755 /etc/
+sudo cp /home/box/web/etc/nginx.conf /etc/nginx/
 sudo /etc/init.d/nginx restart
 sudo gunicorn --bind="0.0.0.0:8080" hello:wsgi_application
+sudo source django_env/bin/activate
+sudo python3 manage.py runserver 0.0.0.0:8000
