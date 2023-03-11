a41c4a5aedda8928eaa04b2c269f8018cb9980b2
AndrewYako <andrejs.jakovlevs.7@inbox.lv>
2023-03-11 11:22:07 +0200

    11.03

--- box/web/init.sh
+++ box/web/init.sh
@@ -1,5 +1,8 @@
+#sudo apt update
+#sudo rm /usr/bin/python3
+#sudo apt install python 3.9
+#sudo pip install --upgrade django
 
-#sudo pip3 install django
 #sudo pip3 install gunicorn
 #sudo apt update
 #sudo apt upgrade
@@ -10,6 +13,10 @@ sudo cp /home/box/web/etc/nginx.conf /etc/nginx/
 #sudo /etc/init.d/nginx restart
 #sudo gunicorn --bind="0.0.0.0:8080" hello:wsgi_application
 #sudo source django_env/bin/activate
+#pip install django
+#pip install gunicorn
 #sudo python3 manage.py migrate
 #sudo python3 manage.py runserver 0.0.0.0:8000
 
+# jobs -l
+# sudo kill -9 ...
