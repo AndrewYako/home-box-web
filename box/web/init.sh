05ed22e0cdf992318d2209eeb47cbd955daaf89e
AndrewYako <andrejs.jakovlevs.7@inbox.lv>
2023-03-11 12:26:10 +0200

    11.03.23

--- box/web/init.sh
+++ box/web/init.sh
@@ -1,22 +1,30 @@
 #sudo apt update
 #sudo rm /usr/bin/python3
-#sudo apt install python 3.9
+#sudo apt install python 3.5
+#sudo ln -s /usr/bin/python3.5 /usr/bin/python3
 #sudo pip install --upgrade django
 
 #sudo pip3 install gunicorn
-#sudo apt update
+sudo apt update
+sudo apt install python3.5
+sudo unlink /usr/bin/python3
+sudo ln -s /usr/bin/python3.5 /usr/bin/python3
 #sudo apt upgrade
-sudo pip3 install --upgrade django
+sudo pip3 install django==2.0
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo cp /home/box/web/etc/nginx.conf /etc/nginx/
-#sudo /etc/init.d/nginx restart
-#sudo gunicorn --bind="0.0.0.0:8080" hello:wsgi_application
-#sudo source django_env/bin/activate
+sudo /etc/init.d/nginx restart
+cd /home/box/web/
+source django_env/bin/activate
+cd /home/box/web/ask/
+python3 manage.py migrate
+gunicorn --bind="0.0.0.0:8080" hello:wsgi_application
+python3 manage.py runserver 0.0.0.0:8000
 #pip install django
 #pip install gunicorn
 #sudo python3 manage.py migrate
-#sudo python3 manage.py runserver 0.0.0.0:8000
+
 
 # jobs -l
 # sudo kill -9 ...
