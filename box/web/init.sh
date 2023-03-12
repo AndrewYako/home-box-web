c6717c2d77c796211fe2b96f519a50e2e206fe95
AndrewYako <andrejs.jakovlevs.7@inbox.lv>
2023-03-12 20:27:37 +0200

    12.03

--- box/web/init.sh
+++ box/web/init.sh
@@ -5,26 +5,27 @@
 #sudo pip install --upgrade django
 
 #sudo pip3 install gunicorn
+sudo chmod 777 /home/
 sudo apt update
 sudo apt install python3.5
 sudo unlink /usr/bin/python3
 sudo ln -s /usr/bin/python3.5 /usr/bin/python3
 #sudo apt upgrade
-sudo pip3 install django==2.0
+#sudo pip3 install django==2.0
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo cp /home/box/web/etc/nginx.conf /etc/nginx/
 sudo /etc/init.d/nginx restart
 cd /home/box/web/
-source django_env/bin/activate
-cd /home/box/web/ask/
-python3 manage.py migrate
-gunicorn --bind="0.0.0.0:8080" hello:wsgi_application
-python3 manage.py runserver 0.0.0.0:8000
-#pip install django
-#pip install gunicorn
+#source django_env/bin/activate
+#cd /home/box/web/ask/
+#sudo pip3 install gunicorn
 #sudo python3 manage.py migrate
+#cd /home/box/web/
+#gunicorn --bind="0.0.0.0:8080" hello:wsgi_application
+#python3 manage.py runserver 0.0.0.0:8000
 
 
-# jobs -l
+
+# jobs -lS
 # sudo kill -9 ...
