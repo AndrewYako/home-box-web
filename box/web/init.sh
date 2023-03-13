161502b3bb211bbb3e31e88098d782fff2a7b0ed
AndrewYako <andrejs.jakovlevs.7@inbox.lv>
2023-03-13 18:55:53 +0200

    13.03

--- box/web/init.sh
+++ box/web/init.sh
@@ -1,31 +1,32 @@
-#sudo apt update
-#sudo rm /usr/bin/python3
-#sudo apt install python 3.5
-#sudo ln -s /usr/bin/python3.5 /usr/bin/python3
-#sudo pip install --upgrade django
 
-#sudo pip3 install gunicorn
 sudo chmod 777 /home/
 sudo apt update
-sudo apt install python3.5
-sudo unlink /usr/bin/python3
-sudo ln -s /usr/bin/python3.5 /usr/bin/python3
-#sudo apt upgrade
-#sudo pip3 install django==2.0
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo cp /home/box/web/etc/nginx.conf /etc/nginx/
 sudo /etc/init.d/nginx restart
-cd /home/box/web/
-#source django_env/bin/activate
-#cd /home/box/web/ask/
-#sudo pip3 install gunicorn
-#sudo python3 manage.py migrate
-#cd /home/box/web/
-#gunicorn --bind="0.0.0.0:8080" hello:wsgi_application
-#python3 manage.py runserver 0.0.0.0:8000
+
+sudo apt purge -y python2.7-minimal
+sudo apt install python3.5
+sudo unlink /usr/bin/python3
+sudo ln -s /usr/bin/python3.5 /usr/bin/python3
+sudo ln -s /usr/bin/python3.5 /usr/bin/python
+sudo python3.5 get-pip.py --force-reinstall
+sudo apt install -y python3-pip
+sudo ln -s /usr/bin/pip3 /usr/bin/pip
+sudo pip3 install --upgrade pip
+sudo pip3 install --upgrade django==2.1
+sudo pip3 install --upgrade gunicorn
+source /home/box/web/django_env/bin/activate
+gunicorn --bind="0.0.0.0:8080" hello:wsgi_application
+cd /home/box/web/ask/
+sudo python3 manage.py migrate
+sudo python3 manage.py runserver 0.0.0.0:8000
 
 
 
+
+#python --version
+#pip --version
 # jobs -lS
 # sudo kill -9 ...
